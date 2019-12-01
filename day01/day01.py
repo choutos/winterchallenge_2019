f73e8b8dae12919f42bb19840c3a9524976e4620
vandrade <andrade.vazquez@gmail.com>
2019-12-01 23:11:02 +0100

    Day 1, part 2

--- day01/day01.py
+++ day01/day01.py
@@ -18,7 +18,7 @@ each module (your puzzle input), then add together all the fuel values.
 '''
 
 def calc_fuel(mass: int) -> int:
-    return mass // 3 -2
+  return mass // 3 -2
 
 assert calc_fuel(12) == 2
 assert calc_fuel(14) == 2
@@ -26,9 +26,47 @@ assert calc_fuel(1969) == 654
 assert calc_fuel(100756) == 33583
 
 with open("input.txt") as f:
-    masses = [int(line.strip()) for line in f]
+  masses = [int(line.strip()) for line in f]
 
-fuel = 0
+fuel_part1 = 0
 for mass in masses:
-    fuel += calc_fuel(mass)
-print(fuel)
+  fuel_part1 += calc_fuel(mass)
+print(fuel_part1)
+
+'''
+For each module mass, calculate its fuel and add it to the total. 
+Then, treat the fuel amount you just calculated as the input mass 
+and repeat the process, continuing until a fuel requirement 
+is zero or negative. For example:
+
+    A module of mass 14 requires 2 fuel. This fuel requires no 
+      further fuel (2 divided by 3 and rounded down is 0, which 
+      would call for a negative fuel), so the total fuel 
+      required is still just 2.
+    At first, a module of mass 1969 requires 654 fuel. Then, this 
+      fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 
+      70 more fuel, which requires 21 fuel, which requires 5 fuel, 
+      which requires no further fuel. So, the total fuel required 
+      for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+    The fuel required by a module of mass 100756 and its fuel is: 
+      33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+'''
+
+def calc_fuel2(mass: int) -> int:
+    total = 0
+    new_fuel = calc_fuel(mass)
+    while new_fuel > 0:
+      total += new_fuel
+      new_fuel = calc_fuel(new_fuel)
+    return total
+
+assert calc_fuel2(14) == 2
+assert calc_fuel2(1969) == 966
+assert calc_fuel2(100756) == 50346
+
+fuel_part2 = 0
+for mass in masses:
+  fuel_part2 += calc_fuel2(mass)
+print(fuel_part2)
+
+     
