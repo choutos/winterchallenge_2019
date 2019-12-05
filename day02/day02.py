74108ed49a5ee10638f1a224f23e2cbc9587bdcd
Víctor Andrade <victor.andrade@alpegagroup.com>
2019-12-05 12:56:53 +0100

    Day2, part 2... finally!

--- day02/day02.py
+++ day02/day02.py
@@ -22,10 +22,21 @@ where the inputs and outputs are, not their values.
 
 Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
 '''
-intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,5,27,2,27,10,31,1,31,9,35,1,35,5,39,1,6,39,43,2,9,43,47,1,5,47,51,2,6,51,55,1,5,55,59,2,10,59,63,1,63,6,67,2,67,6,71,2,10,71,75,1,6,75,79,2,79,9,83,1,83,5,87,1,87,9,91,1,91,9,95,1,10,95,99,1,99,13,103,2,6,103,107,1,107,5,111,1,6,111,115,1,9,115,119,1,119,9,123,2,123,10,127,1,6,127,131,2,131,13,135,1,13,135,139,1,9,139,143,1,9,143,147,1,147,13,151,1,151,9,155,1,155,13,159,1,6,159,163,1,13,163,167,1,2,167,171,1,171,13,0,99,2,0,14,0]
+#intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,5,27,2,27,10,31,1,31,9,35,1,35,5,39,1,6,39,43,2,9,43,47,1,5,47,51,2,6,51,55,1,5,55,59,2,10,59,63,1,63,6,67,2,67,6,71,2,10,71,75,1,6,75,79,2,79,9,83,1,83,5,87,1,87,9,91,1,91,9,95,1,10,95,99,1,99,13,103,2,6,103,107,1,107,5,111,1,6,111,115,1,9,115,119,1,119,9,123,2,123,10,127,1,6,127,131,2,131,13,135,1,13,135,139,1,9,139,143,1,9,143,147,1,147,13,151,1,151,9,155,1,155,13,159,1,6,159,163,1,13,163,167,1,2,167,171,1,171,13,0,99,2,0,14,0]
+inputfile = 'input.txt'
 
-def computer(intcode: list) -> list:
+def getData():
+    with open(inputfile) as file:
+        for line in file:
+            return list(map(int, line.split(",")))
+
+
+def computer(intcode: list, noun: int = None, verb: int = None, alarm=False) -> list:
     pos = 0
+    if noun:
+        intcode[1] = noun
+    if verb:
+        intcode[2] = verb
     while intcode[pos] != 99:
         (opcode, num1, num2, target) = intcode[pos], intcode[pos+1], intcode[pos+2], intcode[pos+3]
         if opcode == 1:
@@ -35,6 +46,8 @@ def computer(intcode: list) -> list:
         else:
             raise RuntimeError("invalid opcode: {intcode[pos]}")
         pos +=4
+    if alarm:
+        return intcode[0]
     return intcode
 
 def alarm(incode: list, noun: int = 12, verb: int = 2) -> list:
@@ -43,16 +56,25 @@ def alarm(incode: list, noun: int = 12, verb: int = 2) -> list:
     new_code = computer(intcode)
     return new_code[0]
 
-def reverse(target, incode):
-    for noun in range(len(intcode)):
-        for verb in range(len(intcode)):
+def reverse(target, intcode):
+    for noun in range(0,100):
+        for verb in range(0,100):
             result = alarm(intcode, noun, verb)
             if result == target:
-                return target
+                return noun, verb
+intcode = getData()
+
+for noun in range(0,100):
+    for verb in range(0,100):
+        data = getData()
+        if computer(data, noun, verb, alarm=True) == 19690720:
+            print("Part2: {}".format(100 * noun + verb))
+            break
 
     
-#print(alarm(intcode))
-print(reverse(19690720,intcode))
+#print(compute(intcode,noun=2,verb=12,alarm=True))
+
+#print(reverse(19690720,intcode))
 assert computer([1,0,0,0,99]) == [2,0,0,0,99]
 assert computer([2,3,0,3,99]) == [2,3,0,6,99]
 assert computer([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
